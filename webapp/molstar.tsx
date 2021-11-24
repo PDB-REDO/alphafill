e9bd6b0ff6151549bffed2052dc68cd7c3dcba57
Maarten L. Hekkelman <maarten@hekkelman.com>
2021-11-24 15:12:02 +0100

    my-preset

--- webapp/molstar.tsx
+++ webapp/molstar.tsx
@@ -2,7 +2,7 @@ import "core-js/stable";
 import 'molstar/build/viewer/molstar.css';
 import { StructureSelection } from "molstar/lib/mol-model/structure";
 import { DownloadStructure } from 'molstar/lib/mol-plugin-state/actions/structure';
-import { StructureRepresentationPresetProvider } from 'molstar/lib/mol-plugin-state/builder/structure/representation-preset';
+import { StructureRepresentationPresetProvider, presetStaticComponent } from 'molstar/lib/mol-plugin-state/builder/structure/representation-preset';
 import { BuiltInTrajectoryFormat } from 'molstar/lib/mol-plugin-state/formats/trajectory';
 import { createPlugin } from 'molstar/lib/mol-plugin-ui';
 import { PluginUIContext } from 'molstar/lib/mol-plugin-ui/context';
@@ -11,10 +11,71 @@ import { PluginConfig } from 'molstar/lib/mol-plugin/config';
 import { PluginLayoutControlsDisplay } from 'molstar/lib/mol-plugin/layout';
 import { Script } from "molstar/lib/mol-script/script";
 import { Asset } from 'molstar/lib/mol-util/assets';
+import { StateObjectRef } from "molstar/lib/mol-state";
 import "regenerator-runtime/runtime";
 
 export { PLUGIN_VERSION as version } from 'molstar/lib/mol-plugin/version';
 export { setDebugMode, setProductionMode } from 'molstar/lib/mol-util/debug';
+
+// --------------------------------------------------------------------
+
+const myPreset = StructureRepresentationPresetProvider({
+    id: 'my-preset',
+    display: {
+        name: 'AlphaFill Preset', group: 'Custom',
+        description: 'Shows polymers as Cartoon, ligands as Ball & Stick, carbohydrates as 3D-SNFG and water molecules semi-transparent.'
+    },
+    params: () => StructureRepresentationPresetProvider.CommonParams,
+    async apply(ref, params, plugin) {
+        const structureCell = StateObjectRef.resolveAndCheck(plugin.state.data, ref);
+        if (!structureCell) return {};
+
+        const components = {
+            polymer: await presetStaticComponent(plugin, structureCell, 'polymer'),
+            ligand: await presetStaticComponent(plugin, structureCell, 'ligand'),
+            nonStandard: await presetStaticComponent(plugin, structureCell, 'non-standard'),
+            branched: await presetStaticComponent(plugin, structureCell, 'branched', { label: 'Carbohydrate' }),
+            water: await presetStaticComponent(plugin, structureCell, 'water'),
+            ion: await presetStaticComponent(plugin, structureCell, 'ion'),
+            lipid: await presetStaticComponent(plugin, structureCell, 'lipid'),
+            coarse: await presetStaticComponent(plugin, structureCell, 'coarse')
+        };
+
+        const structure = structureCell.obj!.data;
+        const cartoonProps = {
+            sizeFactor: structure.isCoarseGrained ? 0.8 : 0.2,
+        };
+
+        // TODO make configurable
+        const waterType = (components.water?.obj?.data?.elementCount || 0) > 50_000 ? 'line' : 'ball-and-stick';
+        const lipidType = (components.lipid?.obj?.data?.elementCount || 0) > 20_000 ? 'line' : 'ball-and-stick';
+
+        const { update, builder, typeParams, color, symmetryColor, ballAndStickColor } = reprBuilder(plugin, params, structure);
+
+        const representations = {
+            polymer: builder.buildRepresentation(update, components.polymer, { type: 'cartoon', typeParams: { ...typeParams, ...cartoonProps }, color: symmetryColor }, { tag: 'polymer' }),
+            ligand: builder.buildRepresentation(update, components.ligand, { type: 'ball-and-stick', typeParams, color, colorParams: ballAndStickColor }, { tag: 'ligand' }),
+            nonStandard: builder.buildRepresentation(update, components.nonStandard, { type: 'ball-and-stick', typeParams, color, colorParams: ballAndStickColor }, { tag: 'non-standard' }),
+            branchedBallAndStick: builder.buildRepresentation(update, components.branched, { type: 'ball-and-stick', typeParams: { ...typeParams, alpha: 0.3 }, color, colorParams: ballAndStickColor }, { tag: 'branched-ball-and-stick' }),
+            branchedSnfg3d: builder.buildRepresentation(update, components.branched, { type: 'carbohydrate', typeParams, color }, { tag: 'branched-snfg-3d' }),
+            water: builder.buildRepresentation(update, components.water, { type: waterType, typeParams: { ...typeParams, alpha: 0.6 }, color, colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'water' }),
+            ion: builder.buildRepresentation(update, components.ion, { type: 'ball-and-stick', typeParams, color, colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'ion' }),
+            lipid: builder.buildRepresentation(update, components.lipid, { type: lipidType, typeParams: { ...typeParams, alpha: 0.6 }, color, colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'lipid' }),
+            coarse: builder.buildRepresentation(update, components.coarse, { type: 'spacefill', typeParams, color: color || 'chain-id' }, { tag: 'coarse' })
+        };
+
+        await update.commit({ revertOnError: false });
+        await StructureRepresentationPresetProvider.updateFocusRepr(plugin, structure, params.theme?.focus?.name, params.theme?.focus?.params);
+
+        return { components, representations };
+    }
+});
+
+// --------------------------------------------------------------------
+
+
+
+
 
 const DefaultViewerOptions = {
 	layoutIsExpanded: true,
@@ -44,6 +105,10 @@ const DefaultViewerOptions = {
 	emdbProvider: PluginConfig.Download.DefaultEmdbProvider.defaultValue,
 };
 type ViewerOptions = typeof DefaultViewerOptions;
+
+
+
+
 
 export class Viewer {
 	plugin: PluginUIContext
@@ -138,6 +203,8 @@ export class Viewer {
 		}
 		else {
 			const loci = StructureSelection.toLociWithSourceUnits(selection);
+
+			this.plugin.managers.structure.hierarchy.toggleVisibility()
 	
 			this.plugin.managers.interactivity.lociSelects.select({ loci });
 			this.plugin.managers.structure.focus.setFromLoci(loci);
