993558994e1b23b0993232a9d9a342c1d56b4a98
Maarten L. Hekkelman <maarten@hekkelman.com>
2025-01-08 12:54:12 +0100

    Update node modules Add alt id = asym id for all atoms of placed compounds

--- webapp/molstar.tsx
+++ webapp/molstar.tsx
@@ -1,31 +1,47 @@
 import "core-js/stable";
 import 'molstar/build/viewer/molstar.css';
 import { StructureSelection } from "molstar/lib/mol-model/structure";
-import { createPlugin } from 'molstar/lib/mol-plugin-ui';
+import { createPluginUI } from 'molstar/lib/mol-plugin-ui';
 import { PluginUIContext } from 'molstar/lib/mol-plugin-ui/context';
-import { DefaultPluginUISpec } from 'molstar/lib/mol-plugin-ui/spec';
+import { DefaultPluginUISpec, PluginUISpec } from 'molstar/lib/mol-plugin-ui/spec';
 import { Script } from "molstar/lib/mol-script/script";
 import { Asset } from 'molstar/lib/mol-util/assets';
+import { PluginConfig } from "molstar/lib/mol-plugin/config";
+import { renderReact18 } from 'molstar/lib/mol-plugin-ui/react18';
 import "regenerator-runtime/runtime";
 
 export { PLUGIN_VERSION as version } from 'molstar/lib/mol-plugin/version';
 export { setDebugMode, setProductionMode } from 'molstar/lib/mol-util/debug';
 
+const AlphaFillMolstarSpec: PluginUISpec = {
+	...DefaultPluginUISpec(),
+	config: [
+		[PluginConfig.VolumeStreaming.Enabled, false],
+	],
+	layout: {
+		initial: {
+			isExpanded: false,
+			showControls: false
+		}
+	},
+	components: {
+		remoteState: 'none'
+	}
+	
+}
+
 export class Viewer {
 	plugin: PluginUIContext
 
 	constructor(element: HTMLElement) {
-		this.plugin = createPlugin(element, {
-			...DefaultPluginUISpec(),
-			layout: {
-				initial: {
-					isExpanded: false,
-					showControls: false
-				}
-			},
-			components: {
-				remoteState: 'none'
-			}
+		this.createPlugin(element);
+	}
+
+	async createPlugin(element: HTMLElement) {
+		this.plugin = await createPluginUI({
+			target: element,
+			spec: AlphaFillMolstarSpec,
+			render: renderReact18
 		});
 	}
 
@@ -35,16 +51,16 @@ export class Viewer {
 
 			const data = await this.plugin.builders.data.rawData({ data: model }, { state: { isGhost: true } });
 			const trajectory = await this.plugin.builders.structure.parseTrajectory(data, 'mmcif');
-	
+
 			await this.plugin.builders.structure.hierarchy.applyPreset(trajectory, 'default', {
 				structure: {
 					name: 'model',
-					params: { }
+					params: {}
 				},
 				showUnitcell: false,
 				representationPreset: 'auto'
 			});
-        });
+		});
 	}
 
 	loadStructureFromUrl(url: string) {
@@ -53,16 +69,16 @@ export class Viewer {
 
 			const data = await this.plugin.builders.data.download({ url: Asset.Url(url), isBinary: false }, { state: { isGhost: true } });
 			const trajectory = await this.plugin.builders.structure.parseTrajectory(data, 'mmcif');
-	
+
 			await this.plugin.builders.structure.hierarchy.applyPreset(trajectory, 'default', {
 				structure: {
 					name: 'model',
-					params: { }
+					params: {}
 				},
 				showUnitcell: false,
 				representationPreset: 'auto'
 			});
-        });
+		});
 	}
 
 	handleResize() {
